bcb215dd904fa0a8c4ee433ff8aa5f2fa433cb72
Leydson17 <96561774+Leydson17@users.noreply.github.com>
2022-01-14 15:47:17 -0400

    Teste

--- curl.php
+++ curl.php
@@ -1,3 +1,12 @@
+
+<!Doctype html>
+<html>
+        <head>
+                <meta charset="UTF - 8">
+                <h1>Alunos </h1>
+        </head>
+
+        <body>
 <?php
         // inicia o curl
         $curl = curl_init();
@@ -5,13 +14,13 @@
         // Define as configurações
         curl_setopt($curl, CURLOPT_URL,"https://www.unigran.br/campogrande/api/index.php/teste/tecnico");
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'post');
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'put');
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'delete');
-        
+      
         // Executa a Requisição
         $output = curl_exec($curl);
 
         // Fecha a conexão
         curl_close($curl); 
 ?>
+        </body>
+</html>
+
