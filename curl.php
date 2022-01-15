0f8792abc7dede11c6884a113d690d6dba5bd3e0
Leydson17 <96561774+Leydson17@users.noreply.github.com>
2022-01-15 16:32:58 -0400

    ...

--- curl.php
+++ curl.php
@@ -5,9 +5,11 @@
         // Define as configurações
         curl_setopt($curl, CURLOPT_URL,"https://www.unigran.br/campogrande/api/index.php/teste/tecnico");
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-       
+        json_decode(curl_exec($curl));  
+        
+        
         // Executa a Requisição
-        $output = curl_exec($curl);
+        //$output = curl_exec($curl);
 
         // Fecha a conexão
         curl_close($curl); 
